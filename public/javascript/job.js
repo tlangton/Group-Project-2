8e78c20f38073de1004191e75ff466775fedd1bd
Juan <whoisju1@gmail.com>
2017-05-07 17:51:35 -0400

    get form data and send through api

---
+++ public/javascript/job.js
@@ -0,0 +1,31 @@
+var btn = $("#submit-btn");
+
+$("#submit-btn").click(function(e) {
+    e.preventDefault();
+
+
+    // get values from the form
+    var companyName = $("#company-name-input").val().trim(),
+        telephoneNum = $("#company-telephone-input").val().trim(),
+        position = $("#position-input").val().trim(),
+        location = $("#location-input").val().trim(),
+        email = $("#email-input").val().trim(),
+        jobDescription = $("#job-description-input").val().trim(),
+        additionalInfo = $("#additional-info-input").val().trim(),
+        status = $("select").find(":selected").val();
+
+    // store form values in inside of an object
+    var formData = {
+        companyName: companyName,
+        telephoneNum: telephoneNum,
+        position: position,
+        location: location,
+        email: email,
+        jobDescription: jobDescription,
+        additionalInfo: additionalInfo,
+        status: status
+    };
+
+    $.post("url", formData);
+
+});
